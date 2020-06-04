151ffacca657122cc58975e569236f96436e41b9
An5H <46046376+An5H@users.noreply.github.com>
2020-06-04 15:07:37 +0530

    Create SortingLinkedListOfZeroesOnesAndTwos.java

---
+++ SortingLinkedListOfZeroesOnesAndTwos.java
@@ -0,0 +1,105 @@
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new LinkedList().segregate(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class LinkedList
+{
+    static Node segregate(Node head)
+    {
+        Node ptr = head;
+        int arr[] = {0, 0, 0};
+        
+        while(ptr != null){
+            arr[ptr.data]++;
+            ptr = ptr.next;
+        }
+        
+        int i = 0;
+        ptr = head;
+        
+        while(ptr != null){
+            if(arr[i] == 0){
+                i++;
+            }
+            else{
+                ptr.data = i;
+                --arr[i];
+                ptr = ptr.next;
+            }
+        }
+        return head;
+    }
+}
+
+
